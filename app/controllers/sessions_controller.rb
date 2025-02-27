72620766d2abd36f04e7c9a7a7fe242418f1df78
James Yoo <james.yoo@kellogg.northwestern.edu>
2025-02-27 03:00:54 +0000

    post-class

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,27 +8,36 @@ class SessionsController < ApplicationController
     # 1. try to find the user by their unique identifier
     @user = User.find_by({"email" => params["email"]})
     # 2. if the user exists -> check if they know their password
-    if @user != nil 
-      if @user["password"] == params["password"]
+    if @user != nil
+      # 3. if they know their password -> login is successful
+      # if @user["password"] == params["password"]
+      if BCrypt::Password.new(@user["password"]) == params["password"]
+
+        # --- begin user session
+        cookies["name"] = "Cookie Monster"
+        cookies["saying"] = "me like cookies..."
+        session["user_id"] = @user["id"]
+
         flash["notice"] = "Welcome."
-        redirect_to "/companies"  
-        
+        redirect_to "/companies"
       else
         flash["notice"] = "Nope."
         redirect_to "/login"
       end
-      
+      # 4. if the user doesn't exist or they don't know their password -> login fails
     else
       flash["notice"] = "Nope."
       redirect_to "/login"
     end
-    # 3. if they know their password -> login is successful
-    # 4. if the user doesn't exist or they don't know their password -> login fails
   end
 
   def destroy
     # logout the user
+    session["user_id"] = nil
+
     flash["notice"] = "Goodbye."
     redirect_to "/login"
   end
 end
+
+
--- scripts/encryption.rb
+++ scripts/encryption.rb
@@ -4,5 +4,14 @@
 
 # TODO:
 # 1. encrypt a secret and puts encrypted string
+password = "puppies"
+encrypted_pw = BCrypt::Password.create(password)
+puts encrypted_pw
+
+
 # 2. prepare encrypted string for testing
+# $2a$12$YiUztFVXqHyYE74rJtjgDeu/qpgfS/qfU.2I8GEySQi77T7PTNqg6
+puts BCrypt::Password.new(encrypted_pw) == password
+puts BCrypt::Password.new(encrypted_pw)
+
 # 3. test secret against prepared encrypted string
